8f4c79def8585ce8ff6cb94d6c80eee56587c1aa
[sgr0691] <sgr0691@gmail.com>
2016-10-17 00:55:53 -0400

    added NetPay.java file

---
+++ NetPay.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class NetPay {
+public static void main(String[] args) {
+		
+		/* Defined scanner */
+		Scanner keyboard = new Scanner(System.in);
+		
+		/* Defined the constants */
+		final double FEDERAL_TAX_PERCENT = 10.00;
+		final double STATE_TAX_PERCENT = 4.5;
+		final double PAY_PER_HOUR = 7.25;
+		
+		/* Declared the variables */
+		int hoursPerWeek;
+		double grossPay;
+		double netPay;
+		double federalTax;
+		double stateTax;
+	
+		/* Tell users to enter hours per week */
+		System.out.print("Hours per Week: \t");
+		hoursPerWeek = keyboard.nextInt();
+		
+		/* Made sure to compute the variables */
+		grossPay = hoursPerWeek * PAY_PER_HOUR;
+		federalTax = ((grossPay * FEDERAL_TAX_PERCENT)/100);
+		stateTax = ((grossPay * STATE_TAX_PERCENT)/100);
+		netPay = grossPay - federalTax - stateTax;
+		
+		/* Display the results for each question */
+		System.out.println("Gross Pay: \t \t" + grossPay);
+		System.out.printf("Net Pay: \t \t" + "%.5f\n", netPay);
+		System.out.println("");
+		System.out.println("Deductions");
+		System.out.println("Federal: \t \t" + federalTax);
+		System.out.println("State: \t \t \t" + stateTax);
+					
+	}
+}
